9ee55662c40c734d88eb842a6e6bbdb8c5a3758c
Fredrik Østrem <1686349+frxstrem@users.noreply.github.com>
2020-05-28 20:59:38 +0200

    Ignore records used for Let's Encrypt DNS challenges

--- dnsconfig.js
+++ dnsconfig.js
@@ -8,11 +8,17 @@ var CLOUDFLARE = NewDnsProvider('cloudflare', 'CLOUDFLAREAPI', {
 // Domains:
 
 D('example.com', REG_NONE, DnsProvider(CLOUDFLARE),
+    // Ignore records used for Let's Encrypt DNS challenges
+    IGNORE('_acme-challenge.**'),
+
     A('@', '192.0.2.33'),
     CNAME('www', 'example.com.')
 );
 
 D('example.net', REG_NONE, DnsProvider(CLOUDFLARE),
+    // Ignore records used for Let's Encrypt DNS challenges
+    IGNORE('_acme-challenge.**'),
+
     ALIAS('@', 'example.com.'),
     CNAME('www', 'example.com.')
 );
